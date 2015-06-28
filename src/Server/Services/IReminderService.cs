d2e955d53c85ac5c62dc33cbe81eb6b4a07d0ee4
rinoreji <mailrino@gmail.com>
2015-06-28 22:18:22 +0530

    Inital commit
    rough skeleton

---
+++ src/Server/Services/IReminderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Gofer.Server.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IReminderService" in both code and config file together.
+    [ServiceContract]
+    public interface IReminderService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ src/Server/Services/ReminderService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Gofer.Server.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ReminderService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ReminderService.svc or ReminderService.svc.cs at the Solution Explorer and start debugging.
+    public class ReminderService : IReminderService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
